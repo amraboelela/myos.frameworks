f8c2aaa284b527b35d1f9650ef6c4cddfd19267a
Amr Aboelela <amraboelela@gmail.com>
2015-02-09 19:44:01 -0800

    DLog();

--- UIKit/UIApplication.m
+++ UIKit/UIApplication.m
@@ -787,7 +787,9 @@ int _UIApplicationMain(int argc, char *argv[], NSString *principalClassName, NSS
     
     // Setting up the screen sleeping ability
     _application->_lastActivityTime = CACurrentMediaTime();
+    DLog();
     _application->_blackScreen = [[UIView alloc] initWithFrame:cr];
+    DLog();
     _application->_blackScreen.backgroundColor = [UIColor blackColor];
     DLog();
     UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:_application
