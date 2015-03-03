22265b133f54afaeb29104aedb9d8c6615d45fa6
Amr Aboelela <amraboelela@gmail.com>
2015-03-03 14:45:42 -0800

    DLog();

--- Foundation/NSThread.m
+++ Foundation/NSThread.m
@@ -382,6 +382,7 @@ GSCurrentThread(void)
     NSThread *thr = pthread_getspecific(thread_object_key);
     if (nil == thr)
     {
+        DLog();
         GSRegisterCurrentThread();
         thr = pthread_getspecific(thread_object_key);
         if ((nil == defaultThread) && IS_MAIN_PTHREAD)
