ac1d29b816f64c11e28656934793b98380599d3c
Amr Aboelela <amraboelela@gmail.com>
2015-03-03 11:48:13 -0800

    [animation performSelector:@selector(removeFromLayer:) withObject:layer waitUntilDone:YES];

--- CoreAnimation/CAAnimation.m
+++ CoreAnimation/CAAnimation.m
@@ -514,9 +514,9 @@ void _CAAnimationApplyAnimationForLayer(CAAnimation *theAnimation, CALayer *laye
     }
     if (animation->_remove) {
         DLog(@"remove animation: %@", animation);
-        //[animation performSelector:@selector(removeFromLayer:) withObject:layer waitUntilDone:YES];
+        [animation performSelector:@selector(removeFromLayer:) withObject:layer waitUntilDone:YES];
         //[animation performSelectorOnMainThread:@selector(removeFromLayer:) withObject:layer waitUntilDone:YES];
-        [animation removeFromLayer:layer];
+        //[animation removeFromLayer:layer];
         
         //_CAAnimationRemove(animation, layer);
         //[layer removeAnimationForKey:animation->keyPath];
