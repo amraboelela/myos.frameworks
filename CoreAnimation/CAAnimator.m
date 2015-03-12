a599381f959e3fa632e8235ee3aeb8783e0d4e53
Amr Aboelela <amraboelela@gmail.com>
2015-03-12 16:20:11 -0700

    //DLog(@"_CAAnimatorFrameCount: %d, fps: %0.0f", _CAAnimatorFrameCount, fps);

--- CoreAnimation/CAAnimator.m
+++ CoreAnimation/CAAnimator.m
@@ -70,7 +70,7 @@ static void reportFPS(BOOL withCondition)
     if (CACurrentMediaTime() - beforeLockTime > _kResetTimeInterval || !withCondition) {
         float fps = _CAAnimatorFrameCount * 1.0 / (CACurrentMediaTime() - previousTimestamp);
         if (_CAAnimatorFrameCount>1) {
-            DLog(@"_CAAnimatorFrameCount: %d, fps: %0.0f", _CAAnimatorFrameCount, fps);
+            //DLog(@"_CAAnimatorFrameCount: %d, fps: %0.0f", _CAAnimatorFrameCount, fps);
         }
         _CAAnimatorFrameCount = 0;
         previousTimestamp = CACurrentMediaTime();
