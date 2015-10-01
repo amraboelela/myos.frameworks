ddd6288e9bdaa41582bf8a0f2bf1146d8f4d8799
Amr Aboelela <amraboelela@gmail.com>
2015-10-01 16:44:17 -0700

    UIScreen.m

--- UIKit/UIScreen.m
+++ UIKit/UIScreen.m
@@ -66,16 +66,16 @@ NSMutableArray *_allScreens = nil;
         //DLog(@"context: %@", context);
         _hScale = context->_width * 1.0 / _kScreenWidth;
 #ifdef NATIVE_APP
-        _vScale = context->_height * 1.0 / _kScreenHeight;
-#else
         _vScale = context->_height * 1.0 / (_kScreenHeight+_kScreenFooter);
+#else
+        _vScale = context->_height * 1.0 / _kScreenHeight;
 #endif
         _scale = MAX(_hScale, _vScale);
         _screenScaleFactor = _scale;
 //#else
 //        _scale = _kScreenScaleFactor;
 //#endif
-        //DLog(@"_scale: %0.1f", _scale);
+        DLog(@"_scale: %0.1f", _scale);
     }
     return self;
 }
