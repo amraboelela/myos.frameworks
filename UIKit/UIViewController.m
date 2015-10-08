6492c9edf148657bc491995a005d892b366fc424
Amr Aboelela <amraboelela@gmail.com>
2015-10-08 14:46:54 -0700

    Revert "Revert "_vScale = context->_height * 1.0 / (_kScreenHeight+_kScreenFooter);""
    This reverts commit d88b1c28343a0d18750168cd4c44c834e4261401.

--- UIKit/UIViewController.m
+++ UIKit/UIViewController.m
@@ -75,12 +75,7 @@ static id _UIViewControllerNearestParentViewControllerThatIsKindOf(UIViewControl
 - (void)loadView
 {
     CGRect frame = [[UIScreen mainScreen] bounds];
-#ifdef NATIVE_APP
-    frame = CGRectMake(frame.origin.x, frame.origin.y + _kStatusBarHeight, frame.size.width, frame.size.height - _kStatusBarHeight - _kScreenFooter);
-#else
-    frame = CGRectMake(frame.origin.x, frame.origin.y + _kStatusBarHeight, frame.size.width, frame.size.height-_kStatusBarHeight);
-    //frame = CGRectMake(frame.origin.x, frame.origin.y+50, frame.size.width, frame.size.height-50);
-#endif
+    frame = CGRectMake(frame.origin.x, frame.origin.y + _kStatusBarHeight, frame.size.width, frame.size.height - _kStatusBarHeight);
     self.view = [[[UIView alloc] initWithFrame:frame] autorelease];
     //DLog(@"frame: %@", NSStringFromCGRect(frame));
 }
