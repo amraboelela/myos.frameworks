630ba2d149c26042296dfd2f7f81cf672e9a0b6c
Amr Aboelela <amraboelela@gmail.com>
2016-05-25 19:50:26 -0700

    printf("\t'%s' has method named '%s' of encoding '%s'\n",                class_getName(clz),                sel_getName(method_getName(method)),                method_getTypeEncoding(method));

--- XCTest/XCAbstractTest.m
+++ XCTest/XCAbstractTest.m
@@ -27,7 +27,30 @@
 - (void)runTest
 {
     [self setup];
-    DLog(@"To Do : run the tests");
+    //DLog(@"To Do : run the tests");
+    
+    Class clz = [self class];
+    unsigned int methodCount = 0;
+    Method *methods = class_copyMethodList(clz, &methodCount);
+    
+    printf("Found %d methods on '%s'\n", methodCount, class_getName(clz));
+    
+    for (unsigned int i = 0; i < methodCount; i++) {
+        Method method = methods[i];
+        
+        printf("\t'%s' has method named '%s' of encoding '%s'\n",
+               class_getName(clz),
+               sel_getName(method_getName(method)),
+               method_getTypeEncoding(method));
+        
+        /**
+         *  Or do whatever you need here...
+         */
+    }
+    
+    free(methods);
+    
+    
     [self tearDown];
 }
 
