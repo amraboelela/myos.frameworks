f45e36058b800fc11518c5e0ebac2d5d51d8b810
Amr Aboelela <amraboelela@gmail.com>
2016-05-27 11:20:51 -0700

    Moved runTest to XCTestCase class

--- XCTest/XCAbstractTest.m
+++ XCTest/XCAbstractTest.m
@@ -26,36 +26,7 @@
 
 - (void)runTest
 {
-    [self setup];
     
-    Class clz = [self class];
-    NSString *className = NSStringFromClass(clz);
-    NSLog(@"Test Suite '%@' started.", className);
-
-    unsigned int methodCount = 0;
-    Method *methods = class_copyMethodList(clz, &methodCount);
-    int count = 0;
-    NSTimeInterval totalTime = 0;
-    for (unsigned int i = 0; i < methodCount; i++) {
-        Method method = methods[i];
-        NSString *methodName = [NSString stringWithFormat:@"%s", sel_getName(method_getName(method))];
-        if ([methodName rangeOfString:@"test"].location == 0) {
-            count++;
-            NSTimeInterval currentTime = [NSDate timeIntervalSinceReferenceDate];
-            NSLog(@"currentTime: %0.0f", currentTime);
-            NSLog(@"Test Case '%@' started.", methodName);
-            SEL selector = NSSelectorFromString(methodName);
-            [self performSelector:selector];
-            totalTime += [NSDate timeIntervalSinceReferenceDate] - currentTime;
-            NSLog(@"Test Case '%@' passed (%0.3f seconds).", methodName, [NSDate timeIntervalSinceReferenceDate] - currentTime);
-        }
-    }
-    NSLog(@"Test Suite '%@' passed.", className);
-    NSLog(@"     Executed %d tests, with 0 failures in %0.3f seconds", count, totalTime);
-    free(methods);
-    
-    
-    [self tearDown];
 }
 
 - (void)tearDown
